c8dba953aafda0859967959b0870fac2df74f2f4
L4136 <35224756+L4136@users.noreply.github.com>
2018-01-21 20:39:52 +0200

    Add files via upload

---
+++ cd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace oliojaluokka
+{ using System;
+
+namespace CarApplication
+{ 
+
+    class cprogram
+    {
+        static void Main(string[] args)
+        {
+            // create a cd instance
+            Cd endless = new Cd();
+            endless.Artist = "Nightwish";
+            endless.Name = "Endless Forms Most Beautiful";
+            endless.Genre = "Symphonic metal";
+            endless.Price = 19.9;
+            // display cd data
+            endless.PrintData();
+        }
+    }
+}
+}
---
+++ cdProgram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace oliojaluokka
+{
+    class Cd
+    {
+            // properties
+            public string Artist { get; set; }
+            public string Name { get; set; }
+            public string Genre { get; set; }
+            public double Price { get; set; }
+            
+
+        // method to display cd data
+        public void PrintData()
+        {
+            Console.WriteLine("CD Data : ");
+            Console.WriteLine("- artist : " + Artist);
+            Console.WriteLine("- name : " + Name);
+            Console.WriteLine("- genre : " + Genre);
+            Console.WriteLine("- price : " + Price );
+
+        }
+
+    }
+}
+   
